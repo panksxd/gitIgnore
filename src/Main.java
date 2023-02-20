931beb1651f32906018e1249b7a9594aa7227ad2
panksxd <panksxd@gmail.com>
2023-02-20 10:39:37 +0100

    getint

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,47 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println(getString("hvad hedder du?"));
+       // System.out.println(getString("hvad hedder du?"));
+
+       // System.out.println(getInt("angiv venligst din skostørelse"));
+        //System.out.println(getInt("angiv venligst din højde"));
+
+        while (true)
+        {
+            switch (getString("angiv dit ønske"))
+            {
+                case "låne bog":
+                    System.out.println("du ønsker at låne en bog");
+                    break;
+
+            }
+            if(getString("q for quit").equals("q"))
+            {
+                break;
+            }
+        }
     }
     public static String getString(String s)
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(s + ": ");
+        System.out.print(s + ": ");
         return scanner.nextLine();
+    }
+    public static int getInt(String s)
+    {
+        int res = 0;
+
+        while (true)
+        {
+            try
+            {
+            res = Integer.parseInt(getString(s));
+            return res;
+            }
+            catch (Exception e)
+            {
+                System.out.println("skriv venligst kun tal");
+            }
+        }
     }
 }
