b9671f5b1118f74e54028a709913811e60ce5190
panksxd <panksxd@gmail.com>
2023-02-20 10:11:15 +0100

    dlaver getString

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-
+        System.out.println(getString("hvad hedder du?"));
+    }
+    public static String getString(String s)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(s + ": ");
+        return scanner.nextLine();
     }
 }
